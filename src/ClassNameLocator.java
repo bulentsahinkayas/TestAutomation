8ce3238f56f51f65b9f5ede4ee0142409d573462
sahinkayasi <bulentsahinkayasi@hotmail.com>
2019-09-28 14:13:27 -0400

    Nineth Commit

--- src/ClassNameLocator.java
+++ src/ClassNameLocator.java
@@ -1,10 +1,16 @@
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ClassNameLocator {
     public static WebDriver driver;
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        invokeChromeBrowser();
+        driver.get("http://www.facebook.com");
+        Thread.sleep(3000);
+        driver.findElement(By.name("firstname")).sendKeys("Bulent");
+        Thread.sleep(3000);
+        driver.findElement(By.name("lastname")).sendKeys("Sahin");
     }
     public static void invokeChromeBrowser() {
 
